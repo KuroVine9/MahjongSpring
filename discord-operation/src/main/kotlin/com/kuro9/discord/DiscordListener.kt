34782d824c2954a1b88096acc8d883a3cea9c778
KuroVine9 <kurovine9@gmail.com>
2024-02-18 12:44:55 +0900

    init: multi module setting

--- discord-operation/src/main/kotlin/com/kuro9/discord/DiscordListener.kt
+++ discord-operation/src/main/kotlin/com/kuro9/discord/DiscordListener.kt
@@ -1,14 +1,21 @@
 package com.kuro9.discord
 
 import lombok.extern.slf4j.Slf4j
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.OnlineStatus
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
+import net.dv8tion.jda.api.events.session.ReadyEvent
+import net.dv8tion.jda.api.events.session.ShutdownEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.components.buttons.Button
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import java.lang.String
+
 
 @Slf4j
 @Component
@@ -25,7 +32,44 @@ class DiscordListener : ListenerAdapter() {
     }
 
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
-        super.onSlashCommandInteraction(event)
+        when (event.getFullCommandName()) {
+            "ping" -> {}
+
+            "revalid" -> {}
+            "file" -> {}
+
+            "add" -> {}
+            "stat season" -> {}
+            "stat month" -> {}
+            "stat entire" -> {}
+            "rank entire" -> {}
+
+            "rank month" -> {}
+
+            "rank season" -> {}
+
+            "machi" -> {}
+            "admin add" -> {}
+            "admin get" -> {}
+            "admin delete" -> {}
+            "game_group add" -> {}
+            "game_group get" -> {}
+            "delete" -> {}
+            "modify" -> {}
+            else -> {}
+        }
+    }
+
+    override fun onButtonInteraction(event: ButtonInteractionEvent) {
+        val buttonId = event.componentId
+    }
+
+    override fun onReady(event: ReadyEvent) {
+        log.info("DiscordListener onReady")
+    }
+
+    override fun onShutdown(event: ShutdownEvent) {
+        log.info("DiscordListener onShutdown")
     }
 
 }
